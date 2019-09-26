215c1119797da37f39ea864fb8e8a2e15b43fd2a
MathPB <matheus.pereira4025@hotmail.com>
2019-09-25 22:26:58 -0300

    Update 22:26 25/09/2019

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,19 +3,47 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car{
-    park(){
-        return 'stopped';
-    }
 
-    drive(){
-        return 'vroom';
-    }
-}
+beforeEach(() => {
+    // Get a list of all accounts
 
-describe('Car',() => {
-    it('can park', () => {
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
+    web3.eth.getAccounts().then(fetchedAccounts => {
+        console.log(fetchedAccounts);
     });
+
+    //  Use one of the accounts to deploy the contract
 });
+
+describe('Inbox', () => {
+    it('deploy a contract', () =>{});
+});
+
+//  LEARNING MOCHA TEST
+
+// class Car{
+//     park(){
+//         return 'stopped';
+//     }
+
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//     car = new Car();
+// });
+
+// describe('Car',() => {
+//     it('can park', () => {
+//         const car = new Car();
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', () =>{
+//         const car = new Car();
+//         assert.equal(car.drive(), 'vroom');
+//     })
+// });
